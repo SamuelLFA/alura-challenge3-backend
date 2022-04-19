aa49da4e391f1d0c9a080733b8a9345fbbd2c233
SamuelLFA <samuellfa3@gmail.com>
2022-04-18 21:23:48 -0300

    first commit

---
+++ src/main/kotlin/com/samuellfa/alurachallenge/AluraChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.samuellfa.alurachallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/samuellfa/alurachallenge/AluraChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.samuellfa.alurachallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
